1f7a344358b29690b0356def3bbe946743b51101
kazsk1 <131496067+kazsk1@users.noreply.github.com>
2026-08-08 18:52:45 +0900

    Convert UNIX TIme to UTC

--- README.md
+++ README.md
@@ -1,78 +1,94 @@
 # timeto
+
+![Go](https://img.shields.io/badge/Go-1.25+-informational)
+![License](https://img.shields.io/badge/license-MIT-blue)
+
 timeto is a simple command-line tool for time difference calculation.
 
 ![clock](images/clock.png)
 
 ## Getting Started
 
-```
+```bash or powershell
 go install github.com/kazsk1/timeto@main
 ```
 
 ## Usage
-1) Change UTC (RFC3339) into target time of abbreviation such as JST, PDT and SGT
-```powershell or go
-timeto [YYYY-MM-DDTHH:MM:SSZ] [TG Abbr]
-```
+1. Convert UTC (RFC3339) to target timezone abbreviations such as JST, SGT, etc.
+   ```bash or powreshell
+   timeto [YYYY-MM-DDTHH:MM:SSZ] [TG Abbr]
+   ```
 
-2) Change original UTC offset into UTC time
-```powershell or go
-timeto [YYYY/MM/DD] [HH:MM:SS] [OG UTC offset]
-timeto [YYYY-MM-DD] [HH:MM:SS] [OG UTC offset]
-timeto [YYYY/MM/DD] [HH:MM] [OG UTC offset]
-timeto [YYYY-MM-DD] [HH:MM] [OG UTC offset]
-```
+2. Convert UTC offset to UTC.
+   ```bash or powershell
+   timeto [YYYY/MM/DD] [HH:MM:SS] [OG UTC offset]
+   timeto [YYYY-MM-DD] [HH:MM:SS] [OG UTC offset]
+   timeto [YYYY/MM/DD] [HH:MM] [OG UTC offset]
+   timeto [YYYY-MM-DD] [HH:MM] [OG UTC offset]
+   ```
 
-3) Change original UTC offset into target UTC offset
-```powershell or go
-timeto [YYYY/MM/DD] [HH:MM:SS] [OG UTC offset] [TG UTC offset]
-timeto [YYYY-MM-DD] [HH:MM:SS] [OG UTC offset] [TG UTC offset]
-timeto [YYYY/MM/DD] [HH:MM] [OG UTC offset] [TG UTC offset]
-timeto [YYYY-MM-DD] [HH:MM] [OG UTC offset] [TG UTC offset]
-```
+3. Convert original UTC offset to target UTC offset.
+   ```bash or powershell
+   timeto [YYYY/MM/DD] [HH:MM:SS] [OG UTC offset] [TG UTC offset]
+   timeto [YYYY-MM-DD] [HH:MM:SS] [OG UTC offset] [TG UTC offset]
+   timeto [YYYY/MM/DD] [HH:MM] [OG UTC offset] [TG UTC offset]
+   timeto [YYYY-MM-DD] [HH:MM] [OG UTC offset] [TG UTC offset]
+   ```
 
-4) Change original time into target time of timezone abbreviations
-```powershell or go
-timeto [YYYY/MM/DD] [HH:MM:SS] [OG UTC offset] [TG UTC offset]
-timeto [YYYY/MM/DD] [HH:MM:SS] [OG Abbr] [TG Abbr]
-timeto [YYYY-MM-DD] [HH:MM:SS] [OG Abbr] [TG Abbr]
-timeto [YYYY/MM/DD] [HH:MM] [OG Abbr] [TG Abbr]
-timeto [YYYY-MM-DD] [HH:MM] [OG Abbr] [TG Abbr]
-```
+4. Convert original timezone abbreviations to target timezone abbreviations.
+   ```bash or powershell
+   timeto [YYYY/MM/DD] [HH:MM:SS] [OG Abbr] [TG Abbr]
+   timeto [YYYY-MM-DD] [HH:MM:SS] [OG Abbr] [TG Abbr]
+   timeto [YYYY/MM/DD] [HH:MM] [OG Abbr] [TG Abbr]
+   timeto [YYYY-MM-DD] [HH:MM] [OG Abbr] [TG Abbr]
+   ```
+
+5. Convert UNIX time to UTC.
+   ```bash or powershell
+   timeto [UNIX time] 
+   ```
 
 ## Examples
 
-1) Just convert UTC (RFC3339) to JST
-```
-timeto 2023-04-22T09:49:59Z JST
-```
-> Conversion  
->  &ensp; Original time: 2023/04/22 09:49:59 UTC  
->  &ensp;&ensp;&ensp; Target time: 2023/04/22 18:49:59 JST  
+1. Convert UTC (RFC3339) to JST.  
+   ```bash or powershell
+   timeto 2023-04-22T09:49:59Z JST
+   ```
+   > Conversion  
+   >  Original time: 2023/04/22 09:49:59 UTC  
+   >    Target time: 2023/04/22 18:49:59 JST  
 
-2) Convert Original time UTC+9 to Target time UTC
-```
-timeto 2023/04/10 09:00 UTC+9
-```
-> Conversion  
->  &ensp; Original time: 2023/04/10 09:00 UTC+9  
->  &ensp;&ensp;&ensp;&ensp;&ensp; UTC time: 2023/04/10 00:00 UTC  
+2. Convert UTC+9 to UTC.  
+   ```bash or powershell
+   timeto 2023/04/10 09:00 UTC+9
+   ```
+   > Conversion  
+   >  Original time: 2023/04/10 09:00 UTC+9  
+   >       UTC time: 2023/04/10 00:00 UTC  
 
-3) Convert Original time UTC-7 to Target time UTC+9
-```
-timeto 2023/04/21 12:59:59 UTC-7 UTC+9
-```
-> Conversion  
->  &ensp; Original time: 2023/04/21 12:59:59 UTC-7  
->  &ensp;&ensp;&ensp; Target time: 2023/04/22 04:59:59 UTC+9  
+3. Convert UTC-7 to UTC+9.
+   ```bash or powershell
+   timeto 2023/04/21 12:59:59 UTC-7 UTC+9
+   ```
+   > Conversion  
+   >  Original time: 2023/04/21 12:59:59 UTC-7  
+   >    Target time: 2023/04/22 04:59:59 UTC+9  
 
-4) Convert Original time PDT to Target time NZST
-```
-timeto 2023-04-21 15:49:42 PDT NZST
-```
-> Conversion  
->  &ensp; Original time: 2023-04-21 15:49:42 PDT  
->  &ensp;&ensp;&ensp; Target time: 2023-04-22 10:49:42 NZST  
+4. Convert PDT to NZST.
+   ```bash or powershell
+   timeto 2023-04-21 15:49:42 PDT NZST
+   ```
+   > Conversion  
+   >  Original time: 2023-04-21 15:49:42 PDT  
+   >    Target time: 2023-04-22 10:49:42 NZST  
+
+5. Convert UNIX time to UTC.
+   ```bash or powershell
+   timeto 1718000000123
+   ```
+   > Conversion
+   >  UNIX time: 1718000000123
+   >   UTC time: 2024-06-10 06:13:20.123 UTC
 
 
 ## License
--- main.go
+++ main.go
@@ -13,8 +13,6 @@ import (
 
 func param(z int) string {
 
-	// https://www.timeanddate.com/time/zones/
-
 	// PST, PDT, ..., SGT, JST ->  UTC-8, UTC-7, ..., UTC+8, UTC+9
 	// pst, pdt, ..., SGT, JST ->  UTC-8, UTC-7, ..., UTC+8, UTC+9
 	x := strings.ToUpper(os.Args[z])
@@ -63,8 +61,31 @@ func param(z int) string {
 
 func main() {
 
-	//fmt.Printf("\n")
 	fmt.Printf("\033[36mConversion\033[0m\n")
+
+	// UNIX Time -> UTC //
+	if len(os.Args) == 2 {
+
+		// test arg
+		// fmt.Println(len(os.Args))
+		// fmt.Println(os.Args[1])
+		
+		// test command 1 // go run main.go 1718000000123
+
+		// 1. Convert Arg to int64  
+		unixMilli, err := strconv.ParseInt(os.Args[1], 10, 64)
+		if err != nil {
+			fmt.Printf("Invalid value: %v\n", err)
+			return
+		}
+
+		// 2. Convert milliseconds to seconds, nanoseconds, and UTC Time.
+		t := time.Unix(unixMilli/1000, (unixMilli%1000)*int64(time.Millisecond)).UTC()
+
+		// 3. Show the outputs as UTC format
+		fmt.Println(" UNIX time:", os.Args[1])
+		fmt.Println("  UTC time:", t.Format("2006-01-02 15:04:05.999 UTC"))
+	}
 
 	if len(os.Args) == 3 && len(os.Args[2]) >= 3 && len(os.Args[2]) <= 6 && param(2) != "false" {
 
@@ -92,11 +113,11 @@ func main() {
 		min, _ := strconv.Atoi(arg2[1])
 		s, _ := strconv.Atoi(str3)
 
-		// test command 1 // go run main.go 2023-04-22T09:49:59Z JST
-		// test command 2 // go run main.go 2023-04-22T09:49:59Z PDT
-		// test command 3 // go run main.go 2023-04-22T09:49:59Z Est
-		// test command 4 // go run main.go 2023-04-22T09:49:59Z cest
-		// test command 5 // go run main.go 2023-04-22T09:49:59Z west
+		// test command 2 // go run main.go 2023-04-22T09:49:59Z JST
+		// test command 3 // go run main.go 2023-04-22T09:49:59Z PDT
+		// test command 4 // go run main.go 2023-04-22T09:49:59Z Est
+		// test command 5 // go run main.go 2023-04-22T09:49:59Z cest
+		// test command 6 // go run main.go 2023-04-22T09:49:59Z west
 
 		year, _ := strconv.Atoi(arg1[0])
 		var m, _ = strconv.Atoi(arg1[1])
@@ -111,8 +132,8 @@ func main() {
 		targetTime := t.In(targetTimeZone).Add(time.Hour * time.Duration(0))
 
 		// Print the original and converted times
-		fmt.Println("  Original time:", t.Format("2006/01/02 15:04:05 MST"))
-		fmt.Println("    Target time:", targetTime.Format("2006/01/02 15:04:05 MST"))
+		fmt.Println(" Original time:", t.Format("2006/01/02 15:04:05 MST"))
+		fmt.Println("   Target time:", targetTime.Format("2006/01/02 15:04:05 MST"))
 
 	}
 
@@ -140,27 +161,27 @@ func main() {
 		s, _ := strconv.Atoi(arg3[2])
 
 		if len(arg1) == 3 {
-			// test command 6 // go run main.go 2023/04/10 09:00 UTC-1
-			// test command 7 // go run main.go 2023/04/10 09:00 UTC
-			// test command 8 // go run main.go 2023/04/10 09:00 UTC+9
-			// test command 9 // go run main.go 2023/04/10 08:00 UTC-2
-			// test command 10 // go run main.go 2023/04/10 08:45 UTC-2
-			// test command 11 // go run main.go 2023/04/10 08:45:39 UTC-2
-			// test command 12 // go run main.go 2023/04/10 09:00 utc-5
-			// test command 13 // go run main.go 2023/04/10 09:00 utc
-			// test command 14 // go run main.go 2023/04/10 09:00 utc+9
-			// test command 15 // go run main.go 2023/04/10 08:00 uTC-6
-			// test command 16 // go run main.go 2023/04/10 08:45 uTC-7
-			// test command 17 // go run main.go 2023/04/10 08:45:39 uTC-8
-			// test command 18 // go run main.go 2023/04/22 09:00 NZST
-			// test command 19 // go run main.go 2023/04/22 09:00 AEDT
-			// test command 20 // go run main.go 2023/04/22 09:00 AEST
-			// test command 21 // go run main.go 2023/04/22 09:00 AWST
-			// test command 22 // go run main.go 2023/04/22 09:00 JST
-			// test command 23 // go run main.go 2023/04/22 09:00 HKT
-			// test command 24 // go run main.go 2023/04/22 09:00 SGT
-			// test command 25 // go run main.go 2023/04/22 09:00 PDt
-			// test command 26 // go run main.go 2023/04/22 09:00:00 pst
+			// test command 7 // go run main.go 2023/04/10 09:00 UTC-1
+			// test command 8 // go run main.go 2023/04/10 09:00 UTC
+			// test command 9 // go run main.go 2023/04/10 09:00 UTC+9
+			// test command 10 // go run main.go 2023/04/10 08:00 UTC-2
+			// test command 11 // go run main.go 2023/04/10 08:45 UTC-2
+			// test command 12 // go run main.go 2023/04/10 08:45:39 UTC-2
+			// test command 13 // go run main.go 2023/04/10 09:00 utc-5
+			// test command 14 // go run main.go 2023/04/10 09:00 utc
+			// test command 15 // go run main.go 2023/04/10 09:00 utc+9
+			// test command 16 // go run main.go 2023/04/10 08:00 uTC-6
+			// test command 17 // go run main.go 2023/04/10 08:45 uTC-7
+			// test command 18 // go run main.go 2023/04/10 08:45:39 uTC-8
+			// test command 19 // go run main.go 2023/04/22 09:00 NZST
+			// test command 20 // go run main.go 2023/04/22 09:00 AEDT
+			// test command 21 // go run main.go 2023/04/22 09:00 AEST
+			// test command 22 // go run main.go 2023/04/22 09:00 AWST
+			// test command 23 // go run main.go 2023/04/22 09:00 JST
+			// test command 24 // go run main.go 2023/04/22 09:00 HKT
+			// test command 25 // go run main.go 2023/04/22 09:00 SGT
+			// test command 26 // go run main.go 2023/04/22 09:00 PDt
+			// test command 27 // go run main.go 2023/04/22 09:00:00 pst
 
 			year, _ := strconv.Atoi(arg1[0])
 			var m, _ = strconv.Atoi(arg1[1])
@@ -173,38 +194,38 @@ func main() {
 
 			if s == 0 {
 				// Print the original and converted times
-				fmt.Println("  Original time:", t.Format("2006/01/02 15:04 MST"))
-				fmt.Println("       UTC time:", utc.Format("2006/01/02 15:04 MST"))
+				fmt.Println(" Original time:", t.Format("2006/01/02 15:04 MST"))
+				fmt.Println("      UTC time:", utc.Format("2006/01/02 15:04 MST"))
 			}
 
 			if s != 0 {
 				// Print the original and converted times
-				fmt.Println("  Original time:", t.Format("2006/01/02 15:04:05 MST"))
-				fmt.Println("       UTC time:", utc.Format("2006/01/02 15:04:05 MST"))
+				fmt.Println(" Original time:", t.Format("2006/01/02 15:04:05 MST"))
+				fmt.Println("      UTC time:", utc.Format("2006/01/02 15:04:05 MST"))
 			}
 
 		} else if len(arg2) == 3 {
-			// test command 27 // go run main.go 2023-04-10 09:00 UTC-1
-			// test command 28 // go run main.go 2023-04-10 09:00 UTC
-			// test command 29 // go run main.go 2023-04-10 09:00 UTC+9
-			// test command 30 // go run main.go 2023-04-10 08:00 UTC-2
-			// test command 31 // go run main.go 2023-04-10 08:45 UTC-2
-			// test command 32 // go run main.go 2023-04-10 08:45:39 UTC-2
-			// test command 33 // go run main.go 2023-04-10 09:00 utc-5
-			// test command 34 // go run main.go 2023-04-10 09:00 utc
-			// test command 35 // go run main.go 2023-04-10 09:00 utc+9
-			// test command 36 // go run main.go 2023-04-10 08:00 uTC-6
-			// test command 37 // go run main.go 2023-04-10 08:45 uTC-7
-			// test command 38 // go run main.go 2023-04-10 08:45:39 uTC-8
-			// test command 39 // go run main.go 2023-04-22 09:00 NZST
-			// test command 40 // go run main.go 2023-04-22 09:00 AEDT
-			// test command 41 // go run main.go 2023-04-22 09:00 AEST
-			// test command 42 // go run main.go 2023-04-22 09:00 AWST
-			// test command 43 // go run main.go 2023-04-22 09:00 JST
-			// test command 44 // go run main.go 2023-04-22 09:00 HKT
-			// test command 45 // go run main.go 2023-04-22 09:00 SGT
-			// test command 46 // go run main.go 2023-04-22 09:00 PDt
-			// test command 47 // go run main.go 2023-04-22 09:00:00 pst
+			// test command 28 // go run main.go 2023-04-10 09:00 UTC-1
+			// test command 29 // go run main.go 2023-04-10 09:00 UTC
+			// test command 30 // go run main.go 2023-04-10 09:00 UTC+9
+			// test command 31 // go run main.go 2023-04-10 08:00 UTC-2
+			// test command 32 // go run main.go 2023-04-10 08:45 UTC-2
+			// test command 33 // go run main.go 2023-04-10 08:45:39 UTC-2
+			// test command 34 // go run main.go 2023-04-10 09:00 utc-5
+			// test command 35 // go run main.go 2023-04-10 09:00 utc
+			// test command 36 // go run main.go 2023-04-10 09:00 utc+9
+			// test command 37 // go run main.go 2023-04-10 08:00 uTC-6
+			// test command 38 // go run main.go 2023-04-10 08:45 uTC-7
+			// test command 39 // go run main.go 2023-04-10 08:45:39 uTC-8
+			// test command 40 // go run main.go 2023-04-22 09:00 NZST
+			// test command 41 // go run main.go 2023-04-22 09:00 AEDT
+			// test command 42 // go run main.go 2023-04-22 09:00 AEST
+			// test command 43 // go run main.go 2023-04-22 09:00 AWST
+			// test command 44 // go run main.go 2023-04-22 09:00 JST
+			// test command 45 // go run main.go 2023-04-22 09:00 HKT
+			// test command 46 // go run main.go 2023-04-22 09:00 SGT
+			// test command 47 // go run main.go 2023-04-22 09:00 PDt
+			// test command 48 // go run main.go 2023-04-22 09:00:00 pst
 
 			year, _ := strconv.Atoi(arg2[0])
 			var m, _ = strconv.Atoi(arg2[1])
@@ -217,14 +238,14 @@ func main() {
 
 			if s == 0 {
 				// Print the original and converted times
-				fmt.Println("  Original time:", t.Format("2006-01-02 15:04 MST"))
-				fmt.Println("       UTC time:", utc.Format("2006-01-02 15:04 MST"))
+				fmt.Println(" Original time:", t.Format("2006-01-02 15:04 MST"))
+				fmt.Println("      UTC time:", utc.Format("2006-01-02 15:04 MST"))
 			}
 
 			if s != 0 {
 				// Print the original and converted times
-				fmt.Println("  Original time:", t.Format("2006-01-02 15:04:05 MST"))
-				fmt.Println("       UTC time:", utc.Format("2006-01-02 15:04:05 MST"))
+				fmt.Println(" Original time:", t.Format("2006-01-02 15:04:05 MST"))
+				fmt.Println("      UTC time:", utc.Format("2006-01-02 15:04:05 MST"))
 			}
 
 		}
@@ -257,21 +278,21 @@ func main() {
 		s, _ := strconv.Atoi(arg3[2])
 
 		if len(arg1) == 3 {
-			// test command 48 // go run main.go 2023/04/09 00:00 UTC UTC+9
-			// test command 49 // go run main.go 2023/04/09 00:00 UTC+1 UTC+9
-			// test command 50 // go run main.go 2023/04/09 00:00 UTC+2 UTC+9
-			// test command 51 // go run main.go 2023/04/09 00:00 UTC+3 UTC+9
-			// test command 52 // go run main.go 2023/04/09 00:00 UTC+4 UTC+9
-			// test command 53 // go run main.go 2023/04/09 00:30:01 UTC UTC+9
-			// test command 54 // go run main.go 2023/04/09 00:00 utc+5 utc+9
-			// test command 55 // go run main.go 2023/04/09 00:00 utc+6 utc+9
-			// test command 56 // go run main.go 2023/04/09 00:00 utc+7 utc+9
-			// test command 57 // go run main.go 2023/04/09 00:00 uTc+8 uTc+9
-			// test command 58 // go run main.go 2023/04/09 00:00 uTc+9 uTc+9
-			// test command 59 // go run main.go 2023/04/09 00:30:01 uTc+10 uTc+9
-			// test command 60 // go run main.go 2023/04/21 12:59:59 UTC-7 UTC+9
-			// test command 61 // go run main.go 2023/04/21 12:59:59 pdt jst
-			// test command 62 // go run main.go 2023/04/21 15:49:42 PDT NZST
+			// test command 49 // go run main.go 2023/04/09 00:00 UTC UTC+9
+			// test command 50 // go run main.go 2023/04/09 00:00 UTC+1 UTC+9
+			// test command 51 // go run main.go 2023/04/09 00:00 UTC+2 UTC+9
+			// test command 52 // go run main.go 2023/04/09 00:00 UTC+3 UTC+9
+			// test command 53 // go run main.go 2023/04/09 00:00 UTC+4 UTC+9
+			// test command 54 // go run main.go 2023/04/09 00:30:01 UTC UTC+9
+			// test command 55 // go run main.go 2023/04/09 00:00 utc+5 utc+9
+			// test command 56 // go run main.go 2023/04/09 00:00 utc+6 utc+9
+			// test command 57 // go run main.go 2023/04/09 00:00 utc+7 utc+9
+			// test command 58 // go run main.go 2023/04/09 00:00 uTc+8 uTc+9
+			// test command 59 // go run main.go 2023/04/09 00:00 uTc+9 uTc+9
+			// test command 60 // go run main.go 2023/04/09 00:30:01 uTc+10 uTc+9
+			// test command 61 // go run main.go 2023/04/21 12:59:59 UTC-7 UTC+9
+			// test command 62 // go run main.go 2023/04/21 12:59:59 pdt jst
+			// test command 63 // go run main.go 2023/04/21 15:49:42 PDT NZST
 
 			year, _ := strconv.Atoi(arg1[0])
 			var m, _ = strconv.Atoi(arg1[1])
@@ -287,32 +308,32 @@ func main() {
 
 			if s == 0 {
 				// Print the original and target times.
-				fmt.Println("  Original time:", t.Format("2006/01/02 15:04 MST"))
-				fmt.Println("    Target time:", targetTime.Format("2006/01/02 15:04 MST"))
+				fmt.Println(" Original time:", t.Format("2006/01/02 15:04 MST"))
+				fmt.Println("   Target time:", targetTime.Format("2006/01/02 15:04 MST"))
 			}
 
 			if s != 0 {
 				// Print the original and target times.
-				fmt.Println("  Original time:", t.Format("2006/01/02 15:04:05 MST"))
-				fmt.Println("    Target time:", targetTime.Format("2006/01/02 15:04:05 MST"))
+				fmt.Println(" Original time:", t.Format("2006/01/02 15:04:05 MST"))
+				fmt.Println("   Target time:", targetTime.Format("2006/01/02 15:04:05 MST"))
 			}
 
 		} else if len(arg2) == 3 {
-			// test command 63 // go run main.go 2023-04-09 00:00 UTC UTC+9
-			// test command 64 // go run main.go 2024-04-09 00:00 UTC+1 UTC+9
-			// test command 65 // go run main.go 2023-04-09 00:00 UTC+2 UTC+9
-			// test command 66 // go run main.go 2023-04-09 00:00 UTC+3 UTC+9
-			// test command 67 // go run main.go 2023-04-09 00:00 UTC+4 UTC+9
-			// test command 68 // go run main.go 2023-04-09 00:30:01 UTC UTC+9
-			// test command 69 // go run main.go 2023-04-09 00:00 utc+5 utc+9
-			// test command 70 // go run main.go 2024-04-09 00:00 utc+6 utc+9
-			// test command 71 // go run main.go 2023-04-09 00:00 utc+7 utc+9
-			// test command 72 // go run main.go 2023-04-09 00:00 uTc+8 uTc+9
-			// test command 73 // go run main.go 2023-04-09 00:00 uTc+9 uTc+9
-			// test command 74 // go run main.go 2023-04-09 00:30:01 uTc+10 uTc+9
-			// test command 75 // go run main.go 2023-04-21 12:59:59 UTC-7 UTC+9
-			// test command 76 // go run main.go 2023-04-21 12:59:59 pdt jst
-			// test command 77 // go run main.go 2023-04-21 15:49:42 PDT NZST
+			// test command 64 // go run main.go 2023-04-09 00:00 UTC UTC+9
+			// test command 65 // go run main.go 2024-04-09 00:00 UTC+1 UTC+9
+			// test command 66 // go run main.go 2023-04-09 00:00 UTC+2 UTC+9
+			// test command 67 // go run main.go 2023-04-09 00:00 UTC+3 UTC+9
+			// test command 68 // go run main.go 2023-04-09 00:00 UTC+4 UTC+9
+			// test command 69 // go run main.go 2023-04-09 00:30:01 UTC UTC+9
+			// test command 70 // go run main.go 2023-04-09 00:00 utc+5 utc+9
+			// test command 71 // go run main.go 2024-04-09 00:00 utc+6 utc+9
+			// test command 72 // go run main.go 2023-04-09 00:00 utc+7 utc+9
+			// test command 73 // go run main.go 2023-04-09 00:00 uTc+8 uTc+9
+			// test command 74 // go run main.go 2023-04-09 00:00 uTc+9 uTc+9
+			// test command 75 // go run main.go 2023-04-09 00:30:01 uTc+10 uTc+9
+			// test command 76 // go run main.go 2023-04-21 12:59:59 UTC-7 UTC+9
+			// test command 77 // go run main.go 2023-04-21 12:59:59 pdt jst
+			// test command 78 // go run main.go 2023-04-21 15:49:42 PDT NZST
 
 			year, _ := strconv.Atoi(arg2[0])
 			var m, _ = strconv.Atoi(arg2[1])
@@ -328,14 +349,14 @@ func main() {
 
 			if s == 0 {
 				// Print the original and target times.
-				fmt.Println("  Original time:", t.Format("2006-01-02 15:04 MST"))
-				fmt.Println("    Target time:", targetTime.Format("2006-01-02 15:04 MST"))
+				fmt.Println(" Original time:", t.Format("2006-01-02 15:04 MST"))
+				fmt.Println("   Target time:", targetTime.Format("2006-01-02 15:04 MST"))
 			}
 
 			if s != 0 {
 				// Print the original and target times.
-				fmt.Println("  Original time:", t.Format("2006-01-02 15:04:05 MST"))
-				fmt.Println("    Target time:", targetTime.Format("2006-01-02 15:04:05 MST"))
+				fmt.Println(" Original time:", t.Format("2006-01-02 15:04:05 MST"))
+				fmt.Println("   Target time:", targetTime.Format("2006-01-02 15:04:05 MST"))
 			}
 
 		}
